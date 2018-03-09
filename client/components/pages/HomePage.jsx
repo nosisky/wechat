eb578aa47373e40ce5872a790911cfdf46c29e47
Nosisky <nosisky@gmail.com>
2018-03-09 02:43:15 +0100

    fix preloader

--- client/components/pages/HomePage.jsx
+++ client/components/pages/HomePage.jsx
@@ -1,56 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import Loader from 'react-loader';
 
 import AuthForm from '../includes/AuthForm';
 import NavBar from '../includes/NavBar';
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loaded: false
-    }
-  }
 
-  componentDidMount() {
-    this.setState({
-      loaded: true
-    })
-  }
+
   render() {
     return (
-      <Loader
-        width={20}
-        radius={50}
-        loaded={this.state.loaded}>
-        <div>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6">
-                <h1
-                  className="hidden-xs"
-                  style={{
-                    fontFamily: 'cursive',
-                    fontSize: 99,
-                    color: 'green',
-                    backgroundColor: 'white',
-                    paddingTop: 80,
-                    marginTop: -1,
-                    marginBottom: 8,
-                    padding: 54,
-                    paddingBottom: 75,
-                    wordBreak: 'break-word',
-                    borderRadius: 10
-                  }}>
-                  Welcome to WeChat!
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1
+              className="hidden-xs"
+              style={{
+                fontFamily: 'cursive',
+                fontSize: 99,
+                color: 'green',
+                backgroundColor: 'white',
+                paddingTop: 80,
+                marginTop: -1,
+                marginBottom: 8,
+                padding: 54,
+                paddingBottom: 75,
+                wordBreak: 'break-word',
+                borderRadius: 10
+              }}>
+              Welcome to WeChat!
             </h1>
-              </div>
-              <AuthForm />
-            </div>
           </div>
+          <AuthForm />
         </div>
-      </Loader>
+      </div>
     );
   }
 }
--- client/public/js/script.js
+++ client/public/js/script.js
@@ -41,3 +41,12 @@ recognition.onresult = function testVoice(event) {
 
   $("#main-text").val(`${previousText} ${transcript}`);
 };
+
+$(document).ready(function ($) {
+  var Body = $('body');
+  Body.addClass('preloader-site');
+});
+$(window).load(function () {
+  $('.preloader-wrapper').fadeOut();
+  $('body').removeClass('preloader-site');
+});
